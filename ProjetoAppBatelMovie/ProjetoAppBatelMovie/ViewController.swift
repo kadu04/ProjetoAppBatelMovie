d08506380de2777968ba0e301a574d8ef241608b
Ricardo Massaki <ricardo29puruba@gmail.com>
2023-10-26 23:10:38 -0300

    criado ProjetoAppBatelMovie e feito alteracao de storyboard para viewcode

---
+++ ProjetoAppBatelMovie/ProjetoAppBatelMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjetoAppBatelMovie
+//
+//  Created by Ricardo Massaki on 26/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
