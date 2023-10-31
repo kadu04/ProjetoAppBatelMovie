8fc46b7e19c6b1e2519c9b00f6bf25ab491fe794
Ricardo Massaki <ricardo29puruba@gmail.com>
2023-10-31 11:56:49 -0300

    criado tela ForgotPasswordViewController

--- ProjetoAppBatelMovie/ProjetoAppBatelMovie/ForgotPassword/ForgotPasswordViewController.swift
+++ ProjetoAppBatelMovie/ProjetoAppBatelMovie/ForgotPassword/ForgotPasswordViewController.swift
@@ -6,24 +6,45 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ForgotPasswordViewController: UIViewController {
+    
+    var screenView = ForgotPasswordScreenView()
+    var auth: Auth?
+    var alert: Alert?
+    
+    override func loadView() {
+        screenView = ForgotPasswordScreenView()
+        view = screenView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.hideKeyboardWhenTappedAround()
+        screenView.delegate(delegate: self)
+        self.auth = Auth.auth()
+        self.navigationItem.hidesBackButton = true
+        alert = Alert(controller: self)
 
-        // Do any additional setup after loading the view.
+    }
+}
+
+extension ForgotPasswordViewController: ForgotPasswordScreenDelegate {
+    func tappedBackButton() {
+        let viewcontroller = LoginViewController()
+        self.navigationController?.popViewController(animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tappedSendButton() {
+        let email = screenView.emailTextField
+        
+        self.auth?.sendPasswordReset(withEmail: email.text ?? "", completion: { error in
+            if let error {
+                self.alert?.configAlert(title: AlertTexts.errorTitle.rawValue, message: AlertTexts.errorEmail.rawValue, secondButton: false)
+            } else {
+                self.alert?.configAlert(title: AlertTexts.succeeded.rawValue, message: AlertTexts.titleSucceeded.rawValue, secondButton: false)
+            }
+        })
     }
-    */
-
 }
