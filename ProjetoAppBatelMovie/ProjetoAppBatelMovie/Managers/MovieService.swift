695a887b0d32825f54351d7761fbb35d991176af
Ricardo Massaki <ricardo29puruba@gmail.com>
2023-11-01 17:44:20 -0300

    criado a tela SearchViewController

--- ProjetoAppBatelMovie/ProjetoAppBatelMovie/Managers/MovieService.swift
+++ ProjetoAppBatelMovie/ProjetoAppBatelMovie/Managers/MovieService.swift
@@ -114,5 +114,32 @@ class MovieService {
         task.resume()
     }
     
+    func getDiscoverMovies(completion: @escaping (Result<[Title], Error>) -> Void) {
+        guard let url = URL(string:
+        "\(Constants.baseURL)/3/discover/movie?api_key=\(Constants.API_KEY)&language=en=US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate") else {return}
+//        guard let url = URL(string: "\(Constants.baseURL)/3/discover/movie?api_key=\(Constants.API_KEY)include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc") else {return}
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            do {
+                let results = try JSONDecoder().decode(MovieTitleResponse.self, from: data)
+                completion(.success(results.results))
+            }
+            catch {
+                completion(.failure(APIError.failedTogetData))
+            }
+        }
+        
+        task.resume()
+        
+    }
+    
 }
 
+
+
+
+//https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc
+
+//eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxM2NjYjQ3MWY2ZGFlNTE4YzM4MGVhMTgwYmNmZjdkMyIsInN1YiI6IjY0MmVjN2E4NTgzNjFiMDExMTU0Nzc1NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.N2p42_YCTTe9uZ9kCvaHgk_Nwwn1H1KIEO0o0uYOeC8
