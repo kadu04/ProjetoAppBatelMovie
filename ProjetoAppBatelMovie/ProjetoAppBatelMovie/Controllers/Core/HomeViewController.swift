488506b7a431c37265200425ff6f7b0661f0430a
Ricardo Massaki <ricardo29puruba@gmail.com>
2023-11-03 18:22:00 -0300

    feito configuracoes na HomeViewController

--- ProjetoAppBatelMovie/ProjetoAppBatelMovie/Controllers/Core/HomeViewController.swift
+++ ProjetoAppBatelMovie/ProjetoAppBatelMovie/Controllers/Core/HomeViewController.swift
@@ -17,6 +17,9 @@ enum Sections: Int {
 
 class HomeViewController: UIViewController {
     
+    private var randomTrendingMovie: Title?
+    private var headerView: HeroHeaderUIView?
+    
     let sectionTitles: [String] = ["Filmes populares", "Tendências da TV", "Popular", "Próximos lançamentos", "Mais votados"]
 
     private let homeFeedTable: UITableView = {
@@ -34,9 +37,25 @@ class HomeViewController: UIViewController {
         homeFeedTable.delegate = self
         homeFeedTable.dataSource = self
         
-        let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
+        headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 500))
         homeFeedTable.tableHeaderView = headerView
+        configureHeroHeaderView()
     
+    }
+    
+    private func configureHeroHeaderView() {
+        
+        MovieService.shared.getTrendingMovies { [weak self] result in
+            switch result {
+            case .success(let titles):
+                let selectedTitle = titles.randomElement()
+                
+                self?.randomTrendingMovie = selectedTitle
+                self?.headerView?.configure(with: TitleViewModel(titleName: selectedTitle?.original_title ?? "", posterURL: selectedTitle?.poster_path ?? ""))
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
     
     private func configureNavBar() {
